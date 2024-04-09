986a6a8c5b64303c0a8a8b9b3ad334121c9af4de
49dev <115186667+49dev@users.noreply.github.com>
2024-04-09 18:49:09 -0400

    Update main.ts
    Better block!

--- main.ts
+++ main.ts
@@ -7,9 +7,20 @@
 
 //% color="#c77398" weight=100
 namespace multicamera {
-    //% block
-    export function triangulate (sprite: Sprite, otherSprite: Sprite) {
-    scene.centerCameraAt((sprite.x + otherSprite.x) / 2, (sprite.y + otherSprite.y) / 2)
+    /**
+     * Triangulates the position of the camera based on locations of 2 sprites.
+     *
+     * @param mySprite The first sprite
+     * @param mySprite2 The second sprite
+     */
+    //% blockId=muticamera_triangulatetwosprites
+    //% block="triangulate $mySprite and $mySprite2"
+    //% mySprite.shadow=variables_get
+    //% mySprite.defl=mySprite
+    //% mySprite2.shadow=variables_get
+    //% mySprite2.defl=mySprite2
+    export function triangulate2Sprites(mySprite: Sprite, mySprite2: Sprite) {
+    scene.centerCameraAt((mySprite.x + mySprite2.x) / 2, (mySprite.y + mySprite2.y) / 2)
     }
 
     // note that Caml casing yields lower case
