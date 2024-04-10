a94853f1523613be250fda3b995f814e95733898
49dev <115186667+49dev@users.noreply.github.com>
2024-04-10 08:37:50 -0400

    Update main.ts
    Added a second block triangulating the position of three sprites

--- main.ts
+++ main.ts
@@ -13,16 +13,35 @@ namespace multicamera {
      * @param mySprite The first sprite
      * @param mySprite2 The second sprite
      */
-    //% blockId=muticamera_triangulatetwosprites
-    //% block="triangulate $mySprite and $mySprite2"
+    //% blockId=multicamera_triangulatetwosprites
+    //% block="triangulate camera $mySprite $mySprite2"
     //% mySprite.shadow=variables_get
     //% mySprite.defl=mySprite
     //% mySprite2.shadow=variables_get
     //% mySprite2.defl=mySprite2
-    export function triangulate2Sprites(mySprite: Sprite, mySprite2: Sprite) {
+    export function triangulatetwosprites(mySprite: Sprite, mySprite2: Sprite) {
     scene.centerCameraAt((mySprite.x + mySprite2.x) / 2, (mySprite.y + mySprite2.y) / 2)
     }
 
     // note that Caml casing yields lower case
     // Triangulates the camera based on two sprites.
+
+    /**
+     * Triangulates the position of the camera based on locations of 3 sprites.
+     *
+     * @param mySprite The first sprite
+     * @param mySprite2 The second sprite
+     * @param mySprite3 The third sprite
+     */
+    //% blockId=multicamera_triangulatethreesprites
+    //% block="triangulate camera $mySprite $mySprite2 $mySprite3"
+    //% mySprite.shadow=variables_get
+    //% mySprite.defl=mySprite
+    //% mySprite2.shadow=variables_get
+    //% mySprite2.defl=mySprite2
+    //% mySprite3.shadow=variables_get
+    //% mySprite3.defl=mySprite3
+    export function triangulatethreesprites(mySprite: Sprite, mySprite2: Sprite, mySprite3: Sprite) {
+    scene.centerCameraAt((mySprite.x + mySprite2.x + mySprite3.x) / 3, (mySprite.y + mySprite2.y + mySprite3.y) / 3)
+    }
 }
